a331956d78a5149ba80189e28667d50525924449
juanesech <juanesech@proton.me>
2024-12-05 23:28:47 +0100

    day_2 [WIP]

--- src/libs/day_2.rs
+++ src/libs/day_2.rs
@@ -5,22 +5,38 @@ pub fn run(input: &str) -> i32 {
 
     for line in read_lines(input).unwrap() {
         let line = line.unwrap();
-        let report_nums: Vec<i32> = line
+        let levels: Vec<i32> = line
             .split(" ")
             .collect::<Vec<&str>>()
             .iter()
             .map(|n| n.parse::<i32>().unwrap_or(0))
             .collect();
 
-        if report_nums.is_sorted_by(|a, b| a <= b && b - a > 1 && b - a <= 3) {
-            println!("Resport: {:?}", report_nums);
+        println!("Levels: {:?}", levels);
+
+        if levels.is_sorted_by(|a, b| a <= b && ((1..=3).contains(&(b - a)))) {
+            println!("Safe report: {:?}", levels);
             safe_reports += 1;
-        } else if report_nums.is_sorted_by(|a, b| a >= b && a - b > 1 && a - b <= 3) {
-            println!("Resport: {:?}", report_nums);
+        } else if levels.is_sorted_by(|a, b| a >= b && ((1..=3).contains(&(a - b)))) {
+            println!("Safe report: {:?}", levels);
             safe_reports += 1;
+        } else {
+            println!("Not a safe report");
         }
     }
 
-    println!("{safe_reports}");
+    println!("Safe reports: {safe_reports}");
     safe_reports
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day1_run() {
+        let input = format!("{}/src/libs/tests/day_2.txt", env!("CARGO_MANIFEST_DIR"));
+        let result = run(&input);
+        assert_eq!(result, 2);
+    }
 }
